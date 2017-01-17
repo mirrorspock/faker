fb05a42b66e37a6c7fdae40e4ea08c582637e703
Kai-Arne Reiter <git@mirrorspock.com>
2017-01-17 15:40:24 +0100

    initial commit

---
+++ lib/faker/star_trek.rb
@@ -0,0 +1,68 @@
+module Faker
+  class StarTrek < Base
+    class << self
+      def character
+        characters.sample
+      end
+
+      def ship_class
+        ship_classes.sample
+      end
+      
+      def planet
+        planets.sample
+      end
+
+      def quote
+        quotes.sample
+      end
+
+      def specie
+        species.sample
+      end
+
+      
+
+      def starships
+        []
+      end
+
+      def ship_classes
+        ['supply ship', 'cruiser', 'patrol ship', 'colony ship', 'fighter', 'cargo ship', 'dreadnaught', 'escort', 'freighter', 'garbage scow', 'shuttle', 'starship', 'transport', 'warship', 'research vessel']
+      end
+      
+      def characters
+        []
+      end
+
+      def planets
+        []
+      end
+
+      def quotes
+        []
+      end
+      
+      def equipment
+        ['Alphanumeric sequencer','Bio-neural gel pack','Chronometer','Coil spanner','Collector','Decoupler',
+          'Descrambler','Dualitic inverter','Duotronic probe','EJ-7 interlock','Emitter module','Flux coupler',
+          'Gravitic caliper','Hyperspanner','Interlink sequencer','Interphasic coil spanner','Interphasic compensator',
+          'Isodine coupler','Isolinear spanner','Isolitic converter','Magna-spanner','Magnetic probe',
+          'Magneton scanner','Mass spectrometer','Micro-resonator','ODN recoupler','Optronic coupler',
+          'Parametric scanner','Phase coil resonator','Phase compensator','Phase decompiler',
+          'Phase matrix recalibrator','Phase modulator','Quantum flux regulator','Sonic driver','Submicron scanner',
+          'Synchronic meter','Thermal regulator','Trident scanner','Veridium injector','Waveform discriminator']
+      end
+
+      def species
+        ['Acamarians','Akritirians','Aldeans','Andorians','Androids','Aquans','Ardanans','Augments',"Ba'ku",'Bajorans',
+          'Betazoids','Bolians','Borg','Brunali','Capellans','Cardassians','Changelings','Denobulans','Enarans',
+          'Ferengi','Holograms','Humans','Ilari','Iotians',"Jem'Hadar" 'Kazon','Klingons','Kriosians','Malcorians',
+          'Mari','Markalians','Mintakans','Ocampa','Organians','Orions','Platonians','Qomar','Romulans','Suliban',
+          'Talaxians','Tellarites','Teplans','Trill','Vhnori','Vidiians','Vori','Vorta','Vulcans','Xindi','Yaderans',
+          'Yridians']
+      end
+
+    end
+  end
+end
---
+++ test/test_faker_star_trek.rb
@@ -0,0 +1,81 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerStarTrek < Test::Unit::TestCase
+  def setup
+    # @characters = Faker::StarTrek.characters
+    # @planets = Faker::StarTrek.planets
+    # @weapontype = Faker::StarTrek.weapontype
+    # @equipment = Faker::StarTrek.equipment
+    # @species = Faker::StarTrek.species
+    # @vehicles = Faker::StarTrek.vehicles
+    # @quotes = Faker::StarTrek.quotes
+    @ship_classes = Faker::StarTrek.ship_classes
+    # @technobabble = Faker::StarTrek.technobabble
+  end
+
+  def test_strings
+    #     assert @characters.size == 33
+    #     @characters.each do |character|
+    #        refute character.to_s.empty?
+    #     end
+    #
+    assert @ship_classes.size == 15
+        @ship_classes.each do |ship_class|
+           refute ship_class.to_s.empty?
+        end
+    #
+    #     assert @planets.size == 20
+    #     @planets.each do |planet|
+    #       refute planet.to_s.empty?
+    #     end
+    #
+    # assert @quotes.size == 30
+    #     @quotes.each do |quote|
+    #        refute quote.to_s.empty?
+    #     end
+    #
+    # assert @species.size == 9
+    #     @species.each do |specie|
+    #        refute specie.to_s.empty?
+    #     end
+    #
+    #     assert @vehicles.size == 21
+    #     @vehicles.each do |vehicle|
+    #        refute vehicle.to_s.empty?
+    #     end
+    #
+    #     assert @wookie_words.size == 22
+    #     @wookie_words.each do |word|
+    #        assert !word.nil?
+    #        assert word != ""
+    #     end
+  end
+
+  # def test_character
+  #   assert Faker::StarWars.character.match(/\w+/)
+  # end
+  #
+  def test_ship_class
+    assert Faker::StarTrek.ship_class.match(/\w+/)
+  end
+  #
+  # def test_planet
+  #   assert Faker::StarWars.planet.match(/\w+/)
+  # end
+  #
+  # def test_quote
+  #   assert Faker::StarWars.quote.match(/\w+/)
+  # end
+  #
+  # def test_specie
+  #   assert Faker::StarWars.specie.match(/\w+/)
+  # end
+  #
+  # def test_vehicle
+  #   assert Faker::StarWars.vehicle.match(/\w+/)
+  # end
+  #
+  # def test_wookie_sentence
+  #   assert Faker::StarWars.wookie_sentence.match(/\w+/)
+  # end
+end
